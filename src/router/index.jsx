8f7137782dfc2aad1014afd4390b004d2cb97fa4
dilshodmurodov <chessmna2908@gmail.com>
2024-02-26 16:10:01 +0500

    feat: added about page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import GeneralLayout from "../layout/GeneralLayout";
 import Home from "../pages/Home";
+import About from "../pages/About";
 
 export default function Router() {
+  const activeLang = localStorage.getItem("lang") || "uz";
+
   return (
     <Routes>
-      <Route path="/" element={<GeneralLayout />}>
+      <Route path={`:${activeLang}/`} element={<GeneralLayout />}>
         <Route index element={<Home />} />
+        <Route path="about_us" element={<About />} />
       </Route>
     </Routes>
   );
