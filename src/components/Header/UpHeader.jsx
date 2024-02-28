3dec93bd6af9c519ac96b5d5ba38046dc239af05
Mirshod <mirshodmurodov67@gmail.com>
2024-02-28 09:36:59 +0500

    fix: in the test mode added

---
+++ src/components/Header/UpHeader.jsx
@@ -0,0 +1,11 @@
+import Container from "../Container";
+
+export default function UpHeader() {
+  return (
+    <Container>
+      <h1 className="text-lg font-semibold text-center">
+        Sayt test rejimida ishlamoqda!
+      </h1>
+    </Container>
+  );
+}
