3e9a828e7c4f4b85ef2f49d13ab6701585b4651e
Mirshod <mirshodmurodov67@gmail.com>
2024-02-29 20:19:04 +0500

    fix: all langs added

--- src/sections/OnlineReceptionView/index.jsx
+++ src/sections/OnlineReceptionView/index.jsx
@@ -1,23 +1,18 @@
+import { useTranslation } from "react-i18next";
 import Container from "../../components/Container";
 
 export default function OnlineReceptionView() {
+  const { t } = useTranslation();
+
   return (
     <Container className="py-20">
-      <h1 className="text-2xl font-medium mb-8">Onlayn qabulxona</h1>
+      <h1 className="text-2xl font-medium mb-8">{t("online_reception")}</h1>
       <p className="text-blue-700 mb-6 text-xl font-medium  ">
-        Hurmatli saytimiz foydalanuvchilari,
+        {t("contact_page.dear_users")}
       </p>
-      <p className="mb-6">
-        {" "}
-        “O'zbekiston temir yo'llari” AJ nomiga murojaatingizni
-        https://pm.gov.uz/uz O'zbekiston Respublikasi Prezidentining virtual
-        qabulxonasining “VAZIRLIK VA IDORA RAHBARLARINING FUQAROLARNI QABUL
-        QILISH JADVALI VA ULARGA MUROJAAT YUBORISH” 2 bo'limi orqali
-        “Birlashmalar va aksiyadorlik jamiyatlar” ichidan “O'zbekiston temir
-        yo'llari” AJ pozitsiyasini tanlab yuborishingiz mumkin.
-      </p>
+      <p className="mb-6">{t("contact_page.content")}</p>
       <p className="text-blue-700 text-xl font-medium">
-        Hurmat bilan, Ma'muriyat.
+        {t("contact_page.honorly")}
       </p>
     </Container>
   );
