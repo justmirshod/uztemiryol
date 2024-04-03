265efb5b6531938e1fb5d4694f5cd7e65d964980
Dilshod Murodov <97824228+dilshodsherzodovich@users.noreply.github.com>
2024-04-03 12:42:50 +0500

    server url chagned

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 const lang = localStorage.getItem("i18nextLng");
-const API_URL = `https://65bb-213-230-125-170.ngrok-free.app${lang === "ru" || lang === "en" ? `/${lang}` : ""
+const API_URL = `https://back.utyekspress.uz/${lang === "ru" || lang === "en" ? `/${lang}` : ""
   }`;
 export { API_URL };
