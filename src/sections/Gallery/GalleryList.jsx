891e7e84d1d8fde5e49c41041137172098d3769d
dilshodmurodov <dilshod.sherzodovich.2004@gmail.com>
2024-04-04 14:29:30 +0500

    fix: gallery photos fixed

--- src/sections/Gallery/GalleryList.jsx
+++ src/sections/Gallery/GalleryList.jsx
@@ -5,68 +5,17 @@ import { formatImgLink } from "../../utils/format-image-link";
 
 function GalleryList() {
   const { gallery, galleryLoading } = useSelector((state) => state.blogs);
-  const photos = [
-    {
-      img: "",
-      title:
-        "https://railway.uz/upload/iblock/64e/64ed1f6faf679aa900f2b17de11a6634.JPG",
-    },
-    {
-      img: "",
-      title:
-        "https://railway.uz/upload/iblock/312/312bba83e6a9c7a1eba101ac79ac54f0.jpg",
-    },
-    {
-      img: "",
-      title:
-        "https://railway.uz/upload/iblock/6fd/6fd664e5102b2702ab92dfc5a9361f48.jpg",
-    },
-    {
-      img: "",
-      title:
-        "https://railway.uz/upload/iblock/b1b/b1bc09b8d689d1e3048ab5eaaba9615b.JPG",
-    },
-    {
-      img: "",
-      title:
-        "https://railway.uz/upload/iblock/f0d/f0d6a0f46b12026bfb6429b83cc2a804.JPG",
-    },
-    {
-      img: "",
-      title:
-        "https://railway.uz/upload/iblock/f53/f5316a6224adde3a9454ab5745baf64e.jpg",
-    },
-    {
-      img: "",
-      title:
-        "https://railway.uz/upload/iblock/64e/64ed1f6faf679aa900f2b17de11a6634.JPG",
-    },
-    {
-      img: "",
-      title:
-        "https://railway.uz/upload/iblock/312/312bba83e6a9c7a1eba101ac79ac54f0.jpg",
-    },
-    {
-      img: "",
-      title:
-        "https://railway.uz/upload/iblock/6fd/6fd664e5102b2702ab92dfc5a9361f48.jpg",
-    },
-    {
-      img: "",
-      title:
-        "https://railway.uz/upload/iblock/b1b/b1bc09b8d689d1e3048ab5eaaba9615b.JPG",
-    },
-    {
-      img: "",
-      title:
-        "https://railway.uz/upload/iblock/f0d/f0d6a0f46b12026bfb6429b83cc2a804.JPG",
-    },
-    {
-      img: "",
-      title:
-        "https://railway.uz/upload/iblock/f53/f5316a6224adde3a9454ab5745baf64e.jpg",
-    },
-  ];
+
+  const renderGalleryPhotos = (images) => {
+    return images?.map((item) => {
+      <img
+        src={formatImgLink(item?.image)}
+        className="w-full h-[500px] object-cover"
+        alt=""
+      />;
+    });
+  };
+
   const renderGalleryItems = () => {
     if (galleryLoading) return <Loader />;
     if (!gallery?.results?.length)
@@ -78,13 +27,7 @@ function GalleryList() {
           className="gallery-item col-span-4 cursor-pointer overflow-hidden"
         >
           <div className="img-box hover:scale-105 transition-all duration-300 ease-in">
-            <img
-              src={formatImgLink(
-                item?.images?.find((_, index) => index === 0)?.image
-              )}
-              className="w-full h-[500px] object-cover"
-              alt=""
-            />
+            {renderGalleryPhotos(item?.images)}
           </div>
         </div>
       );
