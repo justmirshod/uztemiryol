4eb97b13ba68510977763427038fa574b1e29c7d
justmirshod <mirshod.29.murodov.0804@gmail.com>
2025-06-16 10:03:44 +0500

    fix: added tenders

--- src/sections/News/NewsListItem.jsx
+++ src/sections/News/NewsListItem.jsx
@@ -1,30 +1,35 @@
 import React from "react";
 import { formatDate } from "../../utils/format-date";
 import { formatImgLink } from "../../utils/format-image-link";
-import { Link, useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 
 function NewsListItem({ title, images, created_at, key, id }) {
   const activeLang = localStorage.getItem("i18nextLng") || "uz";
+  const { pathname } = useLocation();
 
   const { category } = useParams();
   return (
     <div className="news-item col-span-1" key={key}>
       <div className="shadow-cardShadow hover:bg-[#f7f7f7] hover:shadow-[none] transition-all ease-in duration-200">
-        <div className="news-image h-[300px]">
-          {images.length ? (
-            <img
-              src={formatImgLink(images[0].image)}
-              className="w-full object-cover h-full"
-              alt=""
-            />
-          ) : null}
-        </div>
+        {!pathname.includes("tenders") && (
+          <div className="news-image h-[300px]">
+            {images.length ? (
+              <img
+                src={formatImgLink(images[0].image)}
+                className="w-full object-cover h-full"
+                alt=""
+              />
+            ) : null}
+          </div>
+        )}
+
         <div className="news-text p-7">
           <Link
             to={`/${activeLang}/blog-detail/${category}/${id}`}
             className="text-[1.25rem] leading-8 font-semibold hover:text-[#0459c4] transition-all duration-300 ease-out cursor-pointer"
           >
-            {title.length > 67 ? title.slice(0, 67) + "..." : title}
+            {title?.split(" ")?.slice(0, 10).join(" ")}...
+            {/* {title.length > 67 ? title.slice(0, 67) + "..." : title} */}
           </Link>
         </div>
         <div className="news-details flex items-center px-7 py-4 border-t font-semibold">
--- src/sections/SingleBlogView/view/SingleBlogView.jsx
+++ src/sections/SingleBlogView/view/SingleBlogView.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { getSingleBlog } from "../../../api/blog";
 import { formatImgLink } from "../../../utils/format-image-link";
 import { formatDate } from "../../../utils/format-date";
@@ -16,6 +16,7 @@ import "../index.css";
 
 function SingleBlogView() {
   const { id } = useParams();
+  const { pathname } = useLocation();
   const dispatch = useDispatch();
   const { singleBlog, blogLoading } = useSelector((state) => state.blogs);
 
@@ -28,33 +29,52 @@ function SingleBlogView() {
 
   return (
     <section className="blog-image">
-      <div className="img-box">
-        {singleBlog?.images?.length ? (
-          <Swiper modules={[Navigation, Pagination]} pagination={true}>
-            {singleBlog?.images?.map((item) => (
-              <SwiperSlide>
-                <img
-                  src={formatImgLink(item?.image)}
-                  alt=""
-                  className="border w-full"
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        ) : null}
-      </div>
+      {!pathname?.includes("tenders") && (
+        <div className="img-box">
+          {singleBlog?.images?.length ? (
+            <Swiper modules={[Navigation, Pagination]} pagination={true}>
+              {singleBlog?.images?.map((item) => (
+                <SwiperSlide>
+                  <img
+                    src={formatImgLink(item?.image)}
+                    alt=""
+                    className="border w-full"
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          ) : null}
+        </div>
+      )}
+
       <div className="meta-details mt-8">
         <p className="text-[#bcbcbc] font-semibold ">
           {formatDate(singleBlog?.created_at)}
         </p>
       </div>
-      <p className="blog-title font-bold text-[2rem] text-[#212121]">
+      <p className="blog-title font-bold text-[2rem] text-[#212121]  mb-4">
         {singleBlog?.title}
       </p>
       <div
         dangerouslySetInnerHTML={{ __html: singleBlog?.description }}
         className="text-[#666]"
       ></div>
+      <div className="h-[1px] bg-gray-400 my-4"></div>
+
+      <ul className="flex flex-col gap-2">
+        <li>
+          <strong>Boshlanish sanasi:</strong>
+          <span>{singleBlog?.start_date}</span>
+        </li>
+        <li className="my-2">
+          <strong>Tugash sanasi:</strong>
+          <span>{singleBlog?.dead_line}</span>
+        </li>
+        <li>
+          <strong>Boshlang'ich summa:</strong>
+          <span>{singleBlog?.start_price}</span>
+        </li>
+      </ul>
     </section>
   );
 }
